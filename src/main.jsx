a7756be294b997c5f844233593f9aa3e037c2eab
umutsatir <u.satir2021@gtu.edu.tr>
2024-10-14 22:26:26 +0300

    file hierarchy created

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import {
     BrowserRouter as Router,
@@ -8,11 +6,12 @@ import {
     Route,
     BrowserRouter,
 } from "react-router-dom";
+import Home from "./pages/Home";
 
 createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />} />
+            <Route path="/" element={<Home />} />
         </Routes>
     </BrowserRouter>
 );
