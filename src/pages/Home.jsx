6bbe35ae20a849fd5de53efbd74abeba32b203c8
Mehmet Nuri Başa <basamehmet394@gmail.com>
2024-10-19 18:48:18 +0300

    Added card component
    Added card component and card grid that holds the all cards. Parts of the card component is done.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import "../styles/Home.css";
+import  CardGrid from "../Cards/CardGrid.jsx";
+
 
 function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <CardGrid />
         </div>
     );
 }
