aa56128c2903fa2c4a2da38332e0ca7de36e95a6
umutsatir <u.satir2021@gtu.edu.tr>
2024-10-20 17:03:34 +0300

    page made responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,45 +1,106 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Navbar.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function Navbar() {
     const [searchText, setSearchText] = useState("");
+    const [isHamburger, setIsHamburger] = useState(window.innerWidth <= 1200);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 1200) {
+                setIsHamburger(true);
+            } else {
+                setIsHamburger(false);
+            }
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     const deleteClick = () => {
         setSearchText("");
+    };
+
+    const handleMenuClick = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
         <div>
             <nav className="navbar">
                 <h1 className="fundchainText">Fundchain</h1>
-                <div className="searchContainer">
-                    <input
-                        className="searchInput"
-                        type="text"
-                        placeholder="Search..."
-                        value={searchText}
-                        onChange={(e) => setSearchText(e.target.value)}
-                    />
-                    <div className="deleteIcon">
-                        <a onClick={deleteClick} className="xmark">
-                            <i className="fa-solid fa-xmark"></i>
+                {!isHamburger && (
+                    <>
+                        <div className="searchContainer">
+                            <input
+                                className="searchInput"
+                                type="text"
+                                placeholder="Search..."
+                                value={searchText}
+                                onChange={(e) => setSearchText(e.target.value)}
+                            />
+                            <div className="deleteIcon">
+                                <a onClick={deleteClick} className="xmark">
+                                    <i className="fa-solid fa-xmark"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="navbarButtons">
+                            <button className="startProjectButton">
+                                Start a Project
+                            </button>
+                            <button className="loginButton">Login</button>
+                            <img
+                                className="profileImage"
+                                src="/profilePicture.png"
+                                alt="Profile"
+                            />
+                        </div>
+                    </>
+                )}
+                {isHamburger && (
+                    <div className="hamburger">
+                        <a onClick={handleMenuClick}>
+                            <i className="fa fa-bars"></i>
                         </a>
                     </div>
-                </div>
-                <div className="navbarButtons">
-                    <button className="startProjectButton">
-                        Start a Project
-                    </button>
-                    <button className="loginButton">Login</button>
-                    <img
-                        className="profileImage"
-                        src="/profilePicture.png"
-                        alt="Profile"
-                    />
-                </div>
+                )}
             </nav>
+            {isMenuOpen && (
+                <div className="menu">
+                    <div className="searchContainer">
+                        <input
+                            className="searchInput"
+                            type="text"
+                            placeholder="Search..."
+                            value={searchText}
+                            onChange={(e) => setSearchText(e.target.value)}
+                        />
+                        <div className="deleteIcon">
+                            <a onClick={deleteClick} className="xmark">
+                                <i className="fa-solid fa-xmark"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="navbarButtons">
+                        <button className="startProjectButton">
+                            Start a Project
+                        </button>
+                        <button className="loginButton">Login</button>
+                        <img
+                            className="profileImage"
+                            src="/profilePicture.png"
+                            alt="Profile"
+                        />
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
