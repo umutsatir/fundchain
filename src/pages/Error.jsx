24851b5817a18ed0e835e603b2f7652504042ebc
Mehmet Nuri Başa <basamehmet394@gmail.com>
2024-11-28 18:08:28 +0300

    added error page.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,24 @@
 import '../styles/Error.css';
 
 function Error() {
+
     return (
-        <div className='error-container'>
-            <h1 className='error-message'>404 - Page Not Found</h1>
-            <p className='error-sub-message'>Sorry, the page you are looking for does not exist.</p>
-        </div>
+        
+            <div className='error-container'>
+                <h1 className='error-message'>ERROR</h1>
+                <h1 className='error-404'>404</h1>
+                <h1 className='error-message'>Page Not Found</h1>
+                <p className='error-sub-message'>Sorry, the page you were looking for does not exist.</p>
+
+                <div className="redirecting-signup">
+                    <span className="signup-text">New to Fundchain?</span>
+                    <Link to="/signup" className="signup-link">
+                        Sign up
+                    </Link>
+                </div>
+            </div>
+       
+
     );
 }
 
