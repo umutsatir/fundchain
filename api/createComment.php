eaff86dd45c877be9f3ac1992921e21b0e9ce19d
umutsatir <u.satir2021@gtu.edu.tr>
2025-01-16 04:06:02 +0300

    cors headers fixed

--- api/createComment.php
+++ api/createComment.php
@@ -1,6 +1,6 @@
 <?php	
 // Set the necessary headers
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 
 // Include necessary files
--- api/viewComments.php
+++ api/viewComments.php
@@ -1,7 +1,7 @@
 <?php
 
 // Set the necessary headers
-header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 
 // Include necessary files
