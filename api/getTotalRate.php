c15fb8e11a1e10842b3e7c583d3e31ccddb55e0e
umutsatir <u.satir2021@gtu.edu.tr>
2025-01-17 01:12:48 +0300

    getTotalRate php file bugfix

--- api/getTotalRate.php
+++ api/getTotalRate.php
@@ -4,19 +4,25 @@
     $pdo = (new PDOClass())->connect();
 
     if ($_GET['userId']){
-        $stmt = $pdo->prepare("SELECT * FROM comments WHERE userId = :userId");
+        $stmt = $pdo->prepare("SELECT * FROM projects WHERE userId = :userId");
         $stmt->execute(['userId' => $_GET["userId"]]);
         $projects = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $totalRate = 0;
         $totalRateCount = 0;
         foreach ($projects as $project) {
-            $totalRate += $project['rate'];
-            $totalRateCount++;
+            $stmt = $pdo->prepare("SELECT * FROM comments WHERE projectId = :projectId");
+            $stmt->execute(['projectId' => $project["projectId"]]);
+            $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($comments as $comment) {
+                $totalRate += $comment['rate'];
+                $totalRateCount++;
+            }
         }
         if ($totalRateCount == 0) {
             echo json_encode(array('status' => true, 'data' => 0));
-        }
+        } else {
         echo json_encode(array('status' => true, 'data' => $totalRate / $totalRateCount));
+        }
     } else {
         echo json_encode(array('status' => false, 'message' => 'User does not exist'));
     }
